e2ef72b113499c3ec672f08fd71c9b71257f027b
wasadmin <wasadmin@example.com>
2024-07-12 14:42:10 -0400

    set up express and mongo

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import {MongoClient, ObjectId} from 'mongodb';
+dotenv.config();
+const app = express();
+app.use(cors()); // Enable CORS for all routes
+const PORT = 3000;
+const url = process.env.MONGO_DB_URL
+const dbName = process.env.MONGO_DB
+const collectionName = process.env.MONGO_DB_COLLECTION
+
+// Endpoint to read and send JSON file content
+app.get('/employee_directory', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const employee_directory = await collection.find({}).toArray();
+        res.json(employee_directory);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Network Error");
+    }
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
